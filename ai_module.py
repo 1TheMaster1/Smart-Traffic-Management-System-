1bd9333ac506f6ffaaae09c4d10295d2498720e3
SalmaaKhaled <salmakhaledabdou@gmail.com>
2025-04-26 14:12:38 +0300

    feat: integrate YOLOv8 car detection

--- ai_module.py
+++ ai_module.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import cv2
+import numpy as np
 
 model = YOLO("best.pt")  # Load YOLOv8 model
 
@@ -13,3 +14,17 @@ def capture_frame(ip, port):
   else:
     print("Failed to capture image")
     return None
+  
+
+def detect_cars(frame_path="capture.jpg"):
+  results = model(frame_path)
+  return results[0]
+
+def extract_boxes(result):
+  boxes = []
+  for box in result.boxes:
+    x1, y1, x2, y2 = box.xyxy[0]
+    boxes.append((float(x1), float(y1), float(x2), float(y2))) #convert tensor elements to float
+    print(f"Box coordinates: ({x1}, {y1}, {x2}, {y2})")
+  return boxes
+
